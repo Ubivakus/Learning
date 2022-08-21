d21e26e7d8d54b73d4250cd51745b3f06260eb40
ubivakus <stepanov.alex.2012@gmail.com>
2022-08-21 18:37:01 +0300

    Added 1 Folder

---
+++ Sum_Numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Синтаксис
+Начало
+Определить первое число
+Определить второе число
+Вывести сумму введенных чисел на экран
+Конец
+*/
+
+int numberA = 31;
+int numberB = 54;
+int result = numberA + numberB;
+Console.WriteLine(result);
