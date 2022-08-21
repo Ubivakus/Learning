258a35899ebb940c494b19059dd4f7310e818b13
ubivakus <stepanov.alex.2012@gmail.com>
2022-08-21 18:49:59 +0300

    Added random

--- Sum_Numbers/Program.cs
+++ Sum_Numbers/Program.cs
@@ -7,7 +7,9 @@
 Конец
 */
 
-int numberA = 31;
-int numberB = 54;
+int numberA = new Random().Next(1, 10); // число в диапазоне от 1 до 9
+int numberB = new Random().Next(1, 10);
+Console.WriteLine("Первое число: " + numberA);
+Console.WriteLine("Второе число: " + numberB);
 int result = numberA + numberB;
-Console.WriteLine(result);
+Console.WriteLine("Сумма чисел: " + result);
